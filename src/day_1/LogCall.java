f16a3406508704e395accef8f0f1d8ea7de89ecb
Riaz11-coder <riaz11@hotmail.com>
2020-05-08 15:36:22 -0400

    add LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -1,6 +1,8 @@
+package day_1;
+
 public class LogCall {
+
     public static void main(String[] args) {
         System.out.println("Another One");
-        System.out.println("Hello from the other side");
     }
 }
