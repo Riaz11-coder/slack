984d42e7627aee6bb86715a79ee02e80ed02018d
Riaz11-coder <64184169+Riaz11-coder@users.noreply.github.com>
2020-05-08 15:27:52 -0400

    Update LogCall.java

--- src/LogCall.java
+++ src/LogCall.java
@@ -1,6 +1,6 @@
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Another One");
-        System.ou.println("Hello from the other side");
+        System.out.println("Hello from the other side");
     }
 }
